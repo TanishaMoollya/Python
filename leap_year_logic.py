ccc27f346fb8f3742899c4d6ea24f52c93ed4317
TanishaMoollya <moollyatanisha@gmail.com>
2025-12-15 22:37:03 +0530

    Leap Year Logic

---
+++ leap_year_logic.py
@@ -0,0 +1,16 @@
+#leap year logic
+def is_leap(year):
+    leap=False
+    if year%4==0:
+        if year%100!=0:
+            leap=True
+        elif year%400==0:
+            leap=True
+            
+    else:
+        leap=False
+    return leap
+
+
+year = int(input())
+print(is_leap(year))
